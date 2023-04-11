0671b28baeb8d2e55290fe7c11544475aac68cbe
StevenLiu <stevenliu986@gmail.com>
2023-04-12 09:20:32 +1000

    add a pic and controller

--- src/main/java/com/springlearning/controller/HomeController.java
+++ src/main/java/com/springlearning/controller/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
 }
