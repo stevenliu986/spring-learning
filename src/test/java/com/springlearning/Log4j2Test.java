e99ac9ae809bc812df1fea8e7c8cde8218f7e958
StevenLiu <stevenliu986@gmail.com>
2023-04-17 12:48:57 +1000

    add log4j2 configuration file and testing code

--- src/test/java/com/springlearning/Log4j2Test.java
+++ src/test/java/com/springlearning/Log4j2Test.java
@@ -24,4 +24,18 @@ class Log4j2Test {
         logger.debug("debug信息");
         logger.trace("trace信息");
     }
+
+    @Test
+    void test02(){
+        // 使用配置文件，xml格式
+        // log4j2默认加载类路径（resources）下的log4j2.xml文件中的配置
+        // 具体参考上面提到的log4j2.xml文件
+        Logger logger = LogManager.getLogger(Log4j2Test.class);
+        logger.fatal("fatal信息");
+        logger.error("error信息");
+        logger.warn("warn信息");
+        logger.info("info信息");
+        logger.debug("debug信息");
+        logger.trace("trace信息");
+    }
 }
